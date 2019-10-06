2bc0dc24e3c9533a78daacbcb9df5eddb1e0f2a8
Pamela_Castro <pameperes@gmail.com>
2019-10-06 16:05:01 -0300

    add test case create user

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        username = 'test'
+        email = 'test@api-test.com'
+        password = 'Supersecret*'
+        user = get_user_model().objects.create_user(
+            username = username,
+		    email=email,
+		    password=password
+	    )
+        
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
