63206e95c6ca089e3bc9f35b7df49d68f5bad572
PamelaCastro18 <pameperes@gmail.com>
2019-11-02 19:12:12 -0300

    Home changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/authentication';
+import { withRouter } from 'react-router-dom';
 
-export default class Home extends Component {
+class Home extends Component {
+
+
+
     render() {
-        return (
-            <div>
-                Home Component
-            </div>
-        );
+        const {isAuthenticated, user} = this.props.auth;
+        const authLinks = (
+            <>
+            <p>Usuário autenticado</p>
+            </>
+        )
+      const guestLinks = (
+        <>
+        <p>Usuário autenticado</p>
+        </>
+      )
+        return(
+            
+                <section className="container" id="welcome">
+                    {isAuthenticated ? authLinks : guestLinks}
+                </section>
+          
+        )
     }
 }
+Home.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser })(withRouter(Home));
